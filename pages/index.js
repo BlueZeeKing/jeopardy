c784c17b541b02928173a19e935e7f2a2c57468e
BlueZeeKing24 <52867955+BlueZeeKing@users.noreply.github.com>
2021-12-09 10:25:40 -0500

    Questions done

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import React, { useRef, useState } from 'react'
 import styled from 'styled-components';
 import { motion } from "framer-motion"
 
+import questions from '../questions';
+
 const SquareStyle = styled.div`
   color: #FBBF24;
   width: 100%;
@@ -13,7 +15,7 @@ const SquareStyle = styled.div`
   font-weight: bold;
   background-color: blue;
   border: 3px solid black;
-  font-size: 1.875rem;
+  font-size: 1.5rem;
   text-align: center;
   display: flex;
   flex-direction: column;
@@ -35,7 +37,7 @@ const SquareStyleBig = styled(motion.div)`
   left: 0;
 `
 
-const categories = ['Math', 'Science', 'Engineering', 'English', 'History']
+const categories = Object.keys(questions)
 
 export default function Home() {
   return (
@@ -59,6 +61,8 @@ function Jeopardy(props) {
     height: 0,
     marginLeft: 0,
     marginTop: 0,
+    category: '',
+    score: 0,
   })
 
   function clickHandler(category, value, coords) {
@@ -68,15 +72,31 @@ function Jeopardy(props) {
       height: coords.height,
       left: coords.x,
       top: coords.y,
+      category: category,
+      score: value,
     })
   }
 
+  function disappear() {
+    setState({
+      reveal: false,
+      width: 0,
+      height: 0,
+      left: 0,
+      top: 0,
+      category: '',
+      score: 0,
+    })
+  }
+
+  console.log(state, questions)
+
   return (
     <>
-      <AnimateSquare width={state.width} height={state.height} left={state.left} top={state.top} reveal={state.reveal} question="The third planet to orbit the sun" answer="What is the Earth?" />
       <div className="flex flex-row w-full h-full border-[3px] border-black">
         {categories.map((item) => <Category onClick={clickHandler} key={item} value={item} />)}
       </div>
+      <AnimateSquare disappear={disappear} width={state.width} height={state.height} left={state.left} top={state.top} reveal={state.reveal} question={state.reveal ? questions[state.category][state.score]['question'] : ''} answer={state.reveal ? questions[state.category][state.score]['answer'] : ''} />
     </>
   )
 }
@@ -90,7 +110,7 @@ function Category(props) {
     <div className="w-full">
       <div className="flex flex-col h-full">
         <Square value={props.value} onClick={(item, item2) => { console.log(item, item2) }} />
-        {[100,200,300,400,500].map((item) => <Square key={item} value={item} onClick={handleClick}/>)}
+        {[200,400,600,800,1000].map((item) => <Square key={item} value={item} onClick={handleClick}/>)}
       </div>
     </div>
   )
@@ -117,13 +137,30 @@ function Square(props) {
 function AnimateSquare(props) {
   const [side, setSide] = useState(false) // question = false
 
+  function increment() {
+    if (side) {
+      props.disappear()
+      setSide(false)
+    } else {
+      setSide(true)
+    }
+  }
+
   if (props.reveal) {
     return (
-      <SquareStyleBig onClick={() => setSide(!side)} animate={{ x: [props.left, 10], y: [props.top, 10], width: [props.width, window.innerWidth - 20], height: [props.height, window.innerHeight - 20] }} transition={{ duration: 2 }}>
-        <motion.p className="whitespace-nowrap w-full relative h-full" animate={{ scale: [0.17, 1] }} transition={{ duration: 2 }}><span className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">{side ? props.answer : props.question}</span></motion.p>
+      <SquareStyleBig onClick={increment} animate={{ x: [props.left, 10], y: [props.top, 10], width: [props.width, window.innerWidth - 20], height: [props.height, window.innerHeight - 2], opacity: [1, 1] }} transition={{ duration: 1 }}>
+        <motion.div className="whitespace-nowrap w-full relative h-full" animate={{ scale: [0.5, 1], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.5 }}><div className="w-full absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] whitespace-normal p-5">{side ? props.answer : props.question}</div></motion.div>
       </SquareStyleBig>
     )
   } else {
-    return null
+    try {
+      return (
+        <SquareStyleBig onClick={increment} animate={{ x: window.innerWidth / 2, y: window.innerHeight / 2, width: 0, height: 0, opacity: [1, 0] }} transition={{ duration: 1 }}>
+          <motion.div className="whitespace-nowrap w-full relative h-full" animate={{ scale: [1, 0.5], opacity: [1, 0] }} transition={{ duration: 0.5 }}><div className="w-full absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] whitespace-normal p-5">{side ? props.answer : props.question}</div></motion.div>
+        </SquareStyleBig>
+      )
+    } catch {
+      return null
+    }
   }
 }
