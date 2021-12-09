fbe41b440af65ea4b183bb6bc82ededed7e80650
BlueZeeKing24 <52867955+BlueZeeKing@users.noreply.github.com>
2021-12-09 14:36:29 -0500

    i have made jeoprady

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,7 @@ export default function Home() {
       <Head>
         <title>Jeopardy</title>
         <meta name="description" content="Jeopardy website for engineering club" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       </Head>
 
       <main className="p-6 w-full h-screen bg-blue-light">
@@ -55,6 +56,7 @@ export default function Home() {
 }
 
 function Jeopardy(props) {
+
   const [state, setState] = useState({
     reveal: false,
     width: 0,
@@ -85,7 +87,7 @@ function Jeopardy(props) {
       left: 0,
       top: 0,
       category: '',
-      score: 0,
+      score: state.score,
     })
   }
 
@@ -93,8 +95,11 @@ function Jeopardy(props) {
 
   return (
     <>
-      <div className="flex flex-row w-full h-full border-[3px] border-black">
-        {categories.map((item) => <Category onClick={clickHandler} key={item} value={item} />)}
+      <div className="flex-col flex h-full">
+        <div className="flex flex-row w-full flex-grow border-[3px] border-black">
+            {categories.map((item) => <Category onClick={clickHandler} key={item} value={item} />)}
+        </div>
+        <Scorer value={state.score} />
       </div>
       <AnimateSquare disappear={disappear} width={state.width} height={state.height} left={state.left} top={state.top} reveal={state.reveal} question={state.reveal ? questions[state.category][state.score]['question'] : ''} answer={state.reveal ? questions[state.category][state.score]['answer'] : ''} />
     </>
@@ -109,7 +114,7 @@ function Category(props) {
   return (
     <div className="w-full">
       <div className="flex flex-col h-full">
-        <Square value={props.value} onClick={(item, item2) => { console.log(item, item2) }} />
+        <Square value={props.value} onClick={(item, item2) => { console.log(item, item2) }} text={true} />
         {[200,400,600,800,1000].map((item) => <Square key={item} value={item} onClick={handleClick}/>)}
       </div>
     </div>
@@ -118,17 +123,23 @@ function Category(props) {
 
 function Square(props) {
   const square = useRef(null);
+  const [show, setShow] = useState(true)
 
   function clickHandler() {
-    let coords = square.current.getBoundingClientRect()
+    if (show) {
+      let coords = square.current.getBoundingClientRect()
 
-    props.onClick(props.value, coords)
+      props.onClick(props.value, coords)
+      setShow(false)
+    } else {
+      setShow(true)
+    }
   }
 
   return (
-    <SquareStyle ref={square} onClick={clickHandler} >
+    <SquareStyle text={false} ref={square} onClick={clickHandler} >
       <div className="flex-grow"></div>
-      <p>{props.value}</p>
+      <p>{show ? props.value : ''}</p>
       <div className="flex-grow"></div>
     </SquareStyle>
   )
@@ -148,8 +159,8 @@ function AnimateSquare(props) {
 
   if (props.reveal) {
     return (
-      <SquareStyleBig onClick={increment} animate={{ x: [props.left, 10], y: [props.top, 10], width: [props.width, window.innerWidth - 20], height: [props.height, window.innerHeight - 2], opacity: [1, 1] }} transition={{ duration: 1 }}>
-        <motion.div className="whitespace-nowrap w-full relative h-full" animate={{ scale: [0.5, 1], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.5 }}><div className="w-full absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] whitespace-normal p-5">{side ? props.answer : props.question}</div></motion.div>
+      <SquareStyleBig onClick={increment} animate={{ x: [props.left, 10], y: [props.top, 10], width: [props.width, window.innerWidth - 20], height: [props.height, window.innerHeight - 20], opacity: [1, 1] }} transition={{ duration: 0.7 }}>
+        <motion.div className="whitespace-nowrap w-full relative h-full" animate={{ scale: [0.5, 1], opacity: [0, 1] }} transition={{ duration: 0.3, delay: 0.7 }}><div className="w-full absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] whitespace-normal p-5">{side ? props.answer : props.question}</div></motion.div>
       </SquareStyleBig>
     )
   } else {
@@ -163,4 +174,90 @@ function AnimateSquare(props) {
       return null
     }
   }
+}
+
+function ScoreSquare(props) {
+  const [state, setState] = useState(`Team ${props.name}`)
+
+  function handleChange(e) {
+    setState(e.target.value)
+  }
+
+  return (
+    <SquareStyle>
+      <div className="flex-grow"></div>
+      <input value={state} onChange={handleChange} type="text" className="w-full m-2 outline-none focus:outline-none bg-blue bg-opacity-0 text-center underline focus:text-yellow-200 hover:text-yellow-300" />
+      <div className="flex flex-row mx-2">
+        <div className="flex flex-col h-full flex-grow" onClick={() => { props.subtract(props.index) }}>
+          <div className="flex-grow"></div>
+          <span class="material-icons text-yellow-300 cursor-default">remove</span>
+          <div className="flex-grow"></div>
+        </div>
+        <p className="text-yellow-300">{props.score}</p>
+        <div className="flex flex-col h-full flex-grow" onClick={() => {props.add(props.index)}}>
+          <div className="flex-grow"></div>
+          <span class="material-icons text-yellow-300 cursor-default">add</span>
+          <div className="flex-grow"></div>
+        </div>
+      </div>
+      <p className="text-yellow-300 text-base underline font-normal cursor-pointer m-2 mt-0" onClick={() => {props.remove(props.index)}}>Remove</p>
+      <div className="flex-grow"></div>
+    </SquareStyle>
+  )
+}
+
+function Scorer(props) {
+  const [state, addScore, subtractScore, newTeam, removeTeam] = useScore(3)
+
+  function add(index) {
+    addScore(index, props.value)
+    console.log(index)
+  }
+
+  function subtract(index) {
+    subtractScore(index, props.value)
+    console.log('subtract')
+  }
+
+  return (
+    <div className="flex flex-row border-[3px] border-black mt-4">
+      {state.map((item, index) => <ScoreSquare index={index} add={add} subtract={subtract} name={index + 1} score={item} onClick={console.log} remove={removeTeam} />)}
+      <Square value={<span class="material-icons text-yellow-300 cursor-default">add</span>} onClick={newTeam} />
+    </div>
+  )
+}
+
+function useScore(numTeams) {
+  let teams = []
+  for (let i = 0; i < numTeams; i++) {
+    teams[i] = 0
+  }
+
+  const [state, setState] = useState(teams)
+
+  function addTeam(index, amount) {
+    let stateCopy = JSON.parse(JSON.stringify(state))
+    stateCopy[index] = stateCopy[index] + amount
+    setState(stateCopy)
+  }
+
+  function subtractTeam(index, amount) {
+    let stateCopy = JSON.parse(JSON.stringify(state))
+    stateCopy[index] = stateCopy[index] - amount
+    setState(stateCopy)
+  }
+
+  function newTeam() {
+    let stateCopy = JSON.parse(JSON.stringify(state))
+    stateCopy.push(0)
+    setState(stateCopy)
+  }
+
+  function removeTeam(index) {
+    let stateCopy = JSON.parse(JSON.stringify(state))
+    stateCopy.splice(index, 1)
+    setState(stateCopy)
+  }
+
+  return [state, addTeam, subtractTeam, newTeam, removeTeam]
 }
